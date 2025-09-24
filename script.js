948cb9525df5c3823ca04cd4841ec3c9a673b5ad
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2025-09-24 17:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const bands = [
+	'The Plot in You',
+	'The Devil Wears Prade',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Slod',
+	'Say Anything',
+	'The Midway State',
+	'We Came as Romans',
+	'Counterparts',
+	'Oh Sleeper',
+	'A Skylit Drive',
+	'Anywhere But Here',
+	'An Old Dog'
+];
+function stripArticle(name){
+	return name.replace(/^(a |an |the )/i, '');
+	
+}
+bands.sort((a, b) => {
+	const nameA = stripArticle(a).toLowerCase();
+	const nameB = stripArticle(b).toLowerCase();
+
+	return nameA.localeCompare(nameB);
+});
+
+const ul = document.getElementById('band');
+bands.forEach(band => {
+	const li = document.createElement('li');
+	li.textContent = band;
+	ul.appendChild(li);
+});
