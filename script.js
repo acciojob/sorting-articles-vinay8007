4d67890d902c62a459c2ec92bc812c92f44c427d
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2025-09-24 19:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const bands = [
-	'The Plot in You',
-	'The Devil Wears Prada',
-	'Pierce the Veil',
-	'Norma Jean',
-	'The Bled',
-	'Say Anything',
-	'The Midway State',
-	'We Came as Romans',
-	'Counterparts',
-	'Oh, Sleeper',
-	'A Skylit Drive',
-	'Anywhere But Here',
-	'An Old Dog'
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
 ];
-function stripArticle(name){
-	return name.replace(/^(a |an |the )/i, '');
-	
-}
-bands.sort((a, b) => {
-	const nameA = stripArticle(a).toLowerCase();
-	const nameB = stripArticle(b).toLowerCase();
 
-	return nameA.localeCompare(nameB);
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, '');
+}
+
+bands.sort((a, b) => {
+  const nameA = stripArticle(a).toLowerCase();
+  const nameB = stripArticle(b).toLowerCase();
+  return nameA.localeCompare(nameB);
 });
 
 const ul = document.getElementById('band');
 bands.forEach(band => {
-	const li = document.createElement('li');
-	li.textContent = band;
-	ul.appendChild(li);
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
 });
